d07e8f4b9fb47ccd6108a2cfe2d437d36179676b
Bystrol <michalbystryk14@wp.pl>
2024-12-27 19:15:36 +0100

    chore: disable auth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,23 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+// import { clerkMiddleware } from '@clerk/nextjs/server';
 // import { createRouteMatcher } from '@clerk/nextjs/server';
 import withI18nMiddleware from './middlewares/with-i18n';
 // import { attachLocaleToAuthRedirects } from './middlewares/utils/attach-locale-to-auth-redirects';
 
 // const isPublicRoute = createRouteMatcher(['/:locale/sign-in(.*)', '/:locale/sign-up(.*)']);
 
-export default clerkMiddleware(async (_auth, request) => {
-  // if (!isPublicRoute(request)) {
-  //   try {
-  //     await auth.protect();
-  //   } catch (error) {
-  //     return attachLocaleToAuthRedirects(request);
-  //   }
-  // }
+// export default clerkMiddleware(async (auth, request) => {
+//   if (!isPublicRoute(request)) {
+//     try {
+//       await auth.protect();
+//     } catch (error) {
+//       return attachLocaleToAuthRedirects(request);
+//     }
+//   }
 
-  return withI18nMiddleware(request);
-});
+//   return withI18nMiddleware(request);
+// });
+
+export default withI18nMiddleware
 
 export const config = {
   matcher: [
